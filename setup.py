c4e9ad2f803cddaf3eda6c0456b278409d15cc2e
Marco Nguyen <hoangnt0705@gmail.com>
2024-10-29 23:27:47 +0100

    add files to make the repository pip installable

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='math-quiz',
+    version='0.1.0',    
+    description='A simple math quiz',
+    url='https://github.com/Marco-Nguyen/FAU_DSSS_HW_2',
+    author='Tai Hoang Nguyen',
+    # author_email='shudson@anl.gov',
+    license='Apache License',
+    packages=['math-quiz'],
+    install_requires=['random',
+                      'unittest',                     
+                      ],
+
+    classifiers=[
+        # 'Development Status :: 1 - Planning',
+        # 'Intended Audience :: Science/Research',
+        'License :: Apache License',  
+        'Operating System :: Windows',        
+        # 'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        # 'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3.5',
+    ],
+)
